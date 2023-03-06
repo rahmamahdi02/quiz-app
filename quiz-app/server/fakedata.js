38a4b10d7d45cf16c29806b889b8b2c5303a5144
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-06 14:43:27 -0800

    better undertand data structure

--- quiz-app/server/fakedata.js
+++ quiz-app/server/fakedata.js
@@ -33,3 +33,54 @@ const fakedata =
 }
 
 export default fakedata;
+
+
+
+/* > fakedata=
+{
+    response_code: 0,
+    results: [
+      {
+        category: 'General Knowledge',
+        type: 'boolean',
+        difficulty: 'medium',
+        question: 'Furby was released in 1998.',
+        correct_answer: 'True',
+        incorrect_answers: [Array]
+      },
+      {
+        category: 'General Knowledge',
+        type: 'boolean',
+        difficulty: 'medium',
+        question: 'There are 86400 seconds in a day.',
+        correct_answer: 'True',
+        incorrect_answers: [Array]
+      },
+      {
+        category: 'General Knowledge',
+        type: 'boolean',
+        difficulty: 'medium',
+        question: 'Albert Einstein had trouble with mathematics when he was in school.',
+        correct_answer: 'False',
+        incorrect_answers: [Array]
+      },
+      {
+        category: 'General Knowledge',
+        type: 'boolean',
+        difficulty: 'medium',
+        question: 'Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.',
+        correct_answer: 'True',
+        incorrect_answers: [Array]
+      },
+      {
+        category: 'General Knowledge',
+        type: 'boolean',
+        difficulty: 'medium',
+        question: 'The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.',
+        correct_answer: 'False',
+        incorrect_answers: [Array]
+      }
+    ]
+  }
+  > 
+  */
