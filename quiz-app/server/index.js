0fe5c30c22d979ef6d34c8fc7c32cab1d4a7532c
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-07 16:30:57 -0800

    retry

--- quiz-app/server/index.js
+++ quiz-app/server/index.js
@@ -24,11 +24,11 @@ app.get("/", (req, res) => {
 // Make the GET request for the GAME Api for grabbing all the questions 
 
 app.get("/api/game", async (req, res) => {
-  const url = `https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean`;
+  const url = `https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=booleann`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-    //console.log("Line 27 server.js", data);
+    
     res.send(data);
   } catch (err) {
     console.error("Fetch error: ", err);
@@ -37,9 +37,9 @@ app.get("/api/game", async (req, res) => {
 
   // //hardcode the game response for testing reasons to don't saturate my API call. 
 
-app.get('/api/game', (req, res) =>{
-    res.json(fakedata);
-})
+// app.get('/api/game', (req, res) =>{
+//     res.json(fakedata);
+// })
 
 
 
